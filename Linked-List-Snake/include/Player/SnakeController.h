76c842d324957148ed74d58f68b304ba6713d32e
TonyGunk01 <rishivl@hotmail.com>
2025-11-15 15:49:09 +0530

    Re-spawn Snake

--- Linked-List-Snake/include/Player/SnakeController.h
+++ Linked-List-Snake/include/Player/SnakeController.h
@@ -21,11 +21,13 @@ namespace Player
 		private:
 			const int initial_snake_length = 10;
 			const float movement_frame_duration = 0.1f;
+			const float restart_duration = 2.0f;
 
 			const Vector2i default_position = Vector2i(25, 13);
 			const Direction default_direction = Direction::RIGHT;
 
 			float elapsed_duration;
+			float restart_counter;
 
 			SnakeState current_snake_state;
 			Direction current_snake_direction;
