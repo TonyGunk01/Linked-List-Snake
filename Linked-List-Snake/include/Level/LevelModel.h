ae5cc334206813b1cb9ce34de3eaf57aeefe7f39
TonyGunk01 <rishivl@hotmail.com>
2025-11-17 14:07:28 +0530

    Create Level 2

--- Linked-List-Snake/include/Level/LevelModel.h
+++ Linked-List-Snake/include/Level/LevelModel.h
@@ -31,5 +31,54 @@ namespace Level
 
 			float getCellWidth();
 			float getCellHeight();
+
+            vector<ElementData> level_one_element_list = {};
+
+            vector<ElementData> level_two_element_list =
+            {
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(1, 1)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(2, 1)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(3, 1)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(1, 2)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(1, 3)),
+
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(48, 1)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(48, 2)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(48, 3)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(47, 1)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(46, 1)),
+
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(1, 26)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(1, 25)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(1, 24)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(2, 26)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(3, 26)),
+
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(48, 26)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(48, 25)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(48, 24)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(47, 26)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(46, 26)),
+
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(21, 11)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(22, 11)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(23, 11)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(24, 11)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(25, 11)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(26, 11)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(27, 11)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(28, 11)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(29, 11)),
+
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(21, 15)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(22, 15)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(23, 15)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(24, 15)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(25, 15)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(26, 15)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(27, 15)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(28, 15)),
+                ElementData(ElementType::OBSTACLE, sf::Vector2i(29, 15)),
+            };
 	};
 }
