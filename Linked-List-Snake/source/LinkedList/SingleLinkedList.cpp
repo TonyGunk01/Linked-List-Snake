5ea75e4b2d1ffb4d91152b10ab80c2b47e627706
TonyGunk01 <rishivl@hotmail.com>
2025-11-19 14:07:23 +0530

    Insert At End

--- Linked-List-Snake/source/LinkedList/SingleLinkedList.cpp
+++ Linked-List-Snake/source/LinkedList/SingleLinkedList.cpp
@@ -93,13 +93,14 @@ namespace LinkedList
 
 	void SingleLinkedList::insertNodeAtTail()
 	{
+		linked_list_size++;
 		Node* new_node = createNode();
 		Node* cur_node = head_node;
 
 		if (cur_node == nullptr)
 		{
 			head_node = new_node;
-			new_node->body_part.initialize(node_width, node_height, default_position, default_direction);
+			initializeNode(new_node, nullptr, Operation::TAIL);
 			return;
 		}
 
@@ -109,7 +110,7 @@ namespace LinkedList
 		}
 
 		cur_node->next = new_node;
-		new_node->body_part.initialize(node_width, node_height, getNewNodePosition(cur_node), cur_node->body_part.getDirection());
+		initializeNode(new_node, cur_node, Operation::TAIL);
 	}
 
 	void SingleLinkedList::removeNodeAtHead()
