3dd97cdd839d1cbe3ae042ce056fd76a6dc7caea
TonyGunk01 <rishivl@hotmail.com>
2025-11-23 08:43:48 +0530

    Insert at Beginning - DLL

--- Linked-List-Snake/include/LinkedListLib/DoubleLinked/DoubleLinkedList.h
+++ Linked-List-Snake/include/LinkedListLib/DoubleLinked/DoubleLinkedList.h
@@ -8,30 +8,30 @@ namespace LinkedListLib
 	{
 		class DoubleLinkedList : public LinkedList
 		{
-		protected:
-			virtual Node* createNode() override;
+			protected:
+				virtual Node* createNode() override;
 
-		public:
-			DoubleLinkedList();
-			~DoubleLinkedList();
+			public:
+				DoubleLinkedList();
+				~DoubleLinkedList();
 
 
-			void insertNodeAtTail() override;
-			void insertNodeAtHead() override;
-			void insertNodeAtMiddle() override;
-			void insertNodeAtIndex(int index) override;
-			void shiftNodesAfterInsertion(Node* new_node, Node* cur_node, Node* prev_node);
+				void insertNodeAtTail() override;
+				void insertNodeAtHead() override;
+				void insertNodeAtMiddle() override;
+				void insertNodeAtIndex(int index) override;
+				void shiftNodesAfterInsertion(Node* new_node, Node* cur_node, Node* prev_node);
 
-			void removeNodeAtTail() override;
-			void removeNodeAtHead() override;
-			void removeNodeAtMiddle() override;
-			void removeNodeAt(int index) override;
-			void removeNodeAtIndex(int index);
-			void removeAllNodes() override;
-			void removeHalfNodes() override;
-			void shiftNodesAfterRemoval(Node* cur_node);
+				void removeNodeAtTail() override;
+				void removeNodeAtHead() override;
+				void removeNodeAtMiddle() override;
+				void removeNodeAt(int index) override;
+				void removeNodeAtIndex(int index);
+				void removeAllNodes() override;
+				void removeHalfNodes() override;
+				void shiftNodesAfterRemoval(Node* cur_node);
 
-			Direction reverse() override;
+				Direction reverse() override;
 		};
 	}
 }
